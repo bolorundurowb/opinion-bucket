33b213a6254ba013e0bba13e2b6bb4a3679ca828
bolorundurowb <ogatimo@gmail.com>
2016-11-11 12:46:12 +0100

    feature: adds base server.js scaffolding

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 /**
  * Created by bolorundurowb on 11/11/16.
  */
+
+import express from 'express';
+import dotenv from 'dotenv';
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config({silent: true});
+}
+
