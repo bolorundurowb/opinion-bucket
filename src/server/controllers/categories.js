6b140297d6f5a0b553a471c91e45a9f37b44d067
bolorundurowb <ogatimo@gmail.com>
2017-01-12 15:05:48 +0100

    issue #24 | adds the barebones scaffolding for the controllers

--- src/server/controllers/categories.js
+++ src/server/controllers/categories.js
@@ -1,3 +1,23 @@
 /**
  * Created by bolorundurowb on 1/11/17.
  */
+
+const categoriesCtrl = {
+  getAll: function (req, res) {
+
+  },
+  getOne: function (req, res) {
+
+  },
+  create: function (req, res) {
+
+  },
+  update: function (req, res) {
+
+  },
+  delete: function (req, res) {
+
+  }
+};
+
+module.exports = categoriesCtrl;
