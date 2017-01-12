787643091d460ceb640260eabfc490b1ec1d718a
bolorundurowb <ogatimo@gmail.com>
2017-01-12 15:05:48 +0100

    issue #24 | adds the barebones scaffolding for the controllers

--- src/server/controllers/auth.js
+++ src/server/controllers/auth.js
@@ -1,3 +1,14 @@
 /**
  * Created by bolorundurowb on 1/11/17.
  */
+
+const authCtrl = {
+  signin: function (req, res) {
+
+  },
+  signup: function (req, res) {
+
+  }
+};
+
+module.exports = authCtrl;
--- src/server/controllers/users.js
+++ src/server/controllers/users.js
@@ -1,3 +1,20 @@
 /**
  * Created by bolorundurowb on 1/11/17.
  */
+
+const usersCtrl = {
+  getAll: function (req, res) {
+
+  },
+  getOne: function (req, res) {
+
+  },
+  update: function (req, res) {
+
+  },
+  delete: function (req, res) {
+
+  }
+};
+
+module.exports = usersCtrl;
