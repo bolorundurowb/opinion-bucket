509dd1ba5deb16ede43a3149deb9017b22ae5f4d
bolorundurowb <ogatimo@gmail.com>
2017-01-13 13:11:48 +0100

    issue #24 | completes implementation of categories

--- src/server/controllers/categories.js
+++ src/server/controllers/categories.js
@@ -13,30 +13,69 @@ const categoriesCtrl = {
   getAll: function (req, res) {
     Categories.find(function (err, categories) {
       if (err) {
-        res.status(500).send({error: err});
+        res.status(500).send(err);
       }
       res.status(200).send(categories);
     });
   },
+
   getOne: function (req, res) {
-    Categories.find({_id: req.param.id}, function (err, category) {
+    Categories.find({_id: req.params.id}, function (err, category) {
       if (err) {
-        res.status(500).send({error: err});
-      }
-      if (!category) {
+        res.status(500).send(err);
+      } else if (!category) {
         res.status(400).send({message: 'No category exists with that id'});
       }
       res.status(200).send(category);
     });
   },
+
   create: function (req, res) {
-
+    if (!req.body.title) {
+      res.status(400).send({message: 'The category requires a title'});
+    } else {
+      Categories.findOne({title: req.body.title}, function (err, result) {
+        if (err) {
+          res.status(500).send(err);
+        } else if (result) {
+          res.status(409).send({message: 'A category exists with that title'});
+        } else {
+          const _category = new Categories(req.body);
+          _category.save(function (err, category) {
+            if (err) {
+              res.status(500).send(err);
+            }
+            res.status(200).send(category);
+          });
+        }
+      });
+    }
   },
+
   update: function (req, res) {
-
+    Categories.findById(req.params.id, function (err, category) {
+      if (err) {
+        res.status(500).send(err);
+      } else if (!req.body.title) {
+        res.status(400).send({message: 'The category requires a title'});
+      }
+      category.title = req.body.title;
+      category.save(function (err, _category) {
+        if (err) {
+          res.status(500).send(err);
+        }
+        res.status(200).send(_category);
+      });
+    });
   },
-  delete: function (req, res) {
 
+  delete: function (req, res) {
+    Categories.findByIdAndRemove(req.params.id, function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+      res.status(200).send({message: 'Category successfully removed'});
+    });
   }
 };
 
