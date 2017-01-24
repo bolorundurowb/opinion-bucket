339f3757c56dc2af307db84cd68f1d0640335dc3
bolorundurowb <ogatimo@gmail.com>
2017-01-24 17:52:37 +0100

    feature: adds logic for auth controller

--- src/server/middleware/authentication.js
+++ src/server/middleware/authentication.js
@@ -2,10 +2,22 @@
  * Created by bolorundurowb on 1/24/17.
  */
 
+const jwt = require('jsonwebtoken');
+const config = require('./../../../config/config');
+
 const auth = function (req, res, next) {
   const token = req.headers['x-access-token'] || req.body.token;
   if (token) {
-
+    jwt.verify(token, config.secret, function (err, decoded) {
+      if (err) {
+        res.status(401).send({message: 'Failed to authenticate token.'});
+      } else {
+        req.user = decoded;
+        next();
+      }
+    });
+  } else {
+    res.status(403).send({mesage: 'You need to be logged in to access that information.'});
   }
 };
 
