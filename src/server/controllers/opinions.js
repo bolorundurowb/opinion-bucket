cca6c9be265db866990a5395299e4fb201508dc8
bolorundurowb <ogatimo@gmail.com>
2017-02-07 15:56:30 +0100

    issue #54 |  completes the removal of opinions from users and topics

--- src/server/controllers/opinions.js
+++ src/server/controllers/opinions.js
@@ -32,7 +32,7 @@ const opinionsCtrl = {
 
   create: function (req, res) {
     req.body.author = req.user._id;
-    if (!(req.body.author && req.body.details)) {
+    if (!(req.body.author && req.body.content)) {
       res.status(400).send({message: 'An opinion must have an author and content'});
     } else if (!req.body.topicId) {
       res.status(400).send({message: 'An opinion must have a topic'});
@@ -50,19 +50,23 @@ const opinionsCtrl = {
             } else {
               topic.opinions.push(_opinion._id);
               topic.opinionsLength++;
-              topic.save(function (err, result) {
+              topic.save(function (err) {
                 if (err) {
-                  console.log('Error updating topic with opinion', err, message);
+                    //eslint-disable-next-line
+                  console.log('Error updating topic with opinion', err);
                 } else {
+                    //eslint-disable-next-line
                   console.log('Successfully updated topic with opinion');
                 }
               });
               Users.findById(req.user._id, function (err, user) {
                 user.topics.push(topic._id);
-                user.save(function (err, result) {
+                user.save(function (err) {
                   if (err) {
+                    //eslint-disable-next-line
                     console.log('Error updating user with topic', err.message);
                   } else {
+                    //eslint-disable-next-line
                     console.log('Successfully updated user with topic');
                   }
                 });
@@ -97,41 +101,45 @@ const opinionsCtrl = {
 
   delete: function (req, res) {
     Opinions.findOneAndRemove({_id: req.params.id})
-      .exec(function (err, removed) {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.status(200).send({message: 'Opinion successfully removed'});
-      }
-    });
+      .exec(function (err) {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.status(200).send({message: 'Opinion successfully removed'});
+        }
+      });
 
     Topics.find({
       opinions: {
-        '$in': [mongoose.Types.ObjectId(req.params._id)]
+        '$in': [mongoose.Types.ObjectId(req.params.id)]
       }
     }).exec(function (err, topics) {
       topics.forEach(function (topic) {
-        var index = topic.opinions.indexOf(req.params._id);
-        if (index != -1) {
+        var index = topic.opinions.indexOf(req.params.id);
+        if (index !== -1) {
           topic.opinions.splice(index, 1);
         }
-        topic.save(function (err, result) {
+        topic.save(function (err) {
           if (err) {
+            //eslint-disable-next-line
             console.log('Cannot remove opinion from topic', err);
           } else {
+            //eslint-disable-next-line
             console.log('Successfully removed opinion from topic');
           }
         });
 
         Users.findById(req.user._id, function (err, user) {
           var index = user.topics.indexOf(topic._id);
-          if (index != -1) {
+          if (index !== -1) {
             user.topics.splice(index, 1);
           }
-          user.save(function (err, result) {
+          user.save(function (err) {
             if (err) {
+              //eslint-disable-next-line
               console.log('Cannot remove topic from user', err);
             } else {
+              //eslint-disable-next-line
               console.log('Successfully removed topic from user');
             }
           });
