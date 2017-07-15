cd83024fff3fbaf6c3e56f2d00a18737f593b448
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-07-15 17:42:24 +0100

    issue #97 | add the role model

---
+++ src/server/models/role.js
@@ -0,0 +1,23 @@
+/**
+ * Created by winner-timothybolorunduro on 15/07/2017.
+ */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const roleSchema = new Schema({
+  _id: {
+    type: Number,
+    required: true
+  },
+  name: {
+    type: String,
+    unique: true,
+    uppercase: true,
+    required: true
+  }
+});
+
+const roleModel = mongoose.model('Role', roleSchema);
+
+module.exports = roleModel;
