dbb5632888abcac855ade0bcdd9c44b0b9e8d1e1
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-08-01 11:15:54 +0100

    issue #95 | refactor users tests

--- tests/server/users.spec.js
+++ tests/server/users.spec.js
@@ -50,218 +50,224 @@ describe('Users', function () {
       });
   });
 
-  describe('', function () {
+  describe('retrieval', function () {
     describe('does not allow', function () {
+      it('for a non-existent user to be retrieved with detail', function (done) {
+        server
+          .get('/api/v1/users/507f1f77bcf86cd799439011/full')
+          .set('x-access-token', userToken)
+          .expect(400)
+          .end(function (err, res) {
+            res.status.should.equal(400);
+            res.body.message.should.equal('No user exists with that id');
+            done();
+          });
+      });
+
+      it('for a non-existent user to be retrieved', function (done) {
+        server
+          .get('/api/v1/users/507f1f77bcf86cd799439011')
+          .set('x-access-token', userToken)
+          .expect(400)
+          .end(function (err, res) {
+            res.status.should.equal(400);
+            res.body.message.should.equal('No user exists with that id');
+            done();
+          });
+      });
+    });
+
+    describe('allows', function () {
+      it('for  all users to be retrieved', function (done) {
+        server
+          .get('/api/v1/users')
+          .set('x-access-token', adminToken)
+          .expect(200)
+          .end(function (err, res) {
+            res.status.should.equal(200);
+            res.body.should.be.type('object');
+            // One admin and one registered user
+            res.body.length.should.equal(2);
+            done();
+          });
+      });
+
+      it('for  a user to be retrieved', function (done) {
+        server
+          .get('/api/v1/users/' + id)
+          .set('x-access-token', adminToken)
+          .expect(200)
+          .end(function (err, res) {
+            res.status.should.equal(200);
+            res.body.should.be.type('object');
+            done();
+          });
+      });
+
+      it('for  a user to be retrieved with more detail', function (done) {
+        server
+          .get('/api/v1/users/' + id + '/full')
+          .set('x-access-token', adminToken)
+          .expect(200)
+          .end(function (err, res) {
+            res.status.should.equal(200);
+            res.body.should.be.type('object');
+            done();
+          });
+      });
+    });
+  });
+
+  describe('update', function () {
+    describe('does not allow', function () {
+      it('for  non-existent users to be updated', function (done) {
+        server
+          .put('/api/v1/users/507f1f77bcf86cd799439011')
+          .set('x-access-token', userToken)
+          .send({
+            lastName: 'Woke'
+          })
+          .expect(404)
+          .end(function (err, res) {
+            res.status.should.equal(404);
+            res.body.should.be.type('object');
+            res.body.message.should.equal('No user with that id');
+            done();
+          });
+      });
 
     });
 
     describe('allows', function () {
-
-    });
-  });
-
-  // Retrieval Tests
-  it('allows for all users to be retrieved', function (done) {
-    server
-      .get('/api/v1/users')
-      .set('x-access-token', adminToken)
-      .expect(200)
-      .end(function (err, res) {
-        res.status.should.equal(200);
-        res.body.should.be.type('object');
-        // One admin and one registered user
-        res.body.length.should.equal(2);
-        done();
-      });
-  });
-
-  it('allows for a user to be retrieved', function (done) {
-    server
-      .get('/api/v1/users/' + id)
-      .set('x-access-token', adminToken)
-      .expect(200)
-      .end(function (err, res) {
-        res.status.should.equal(200);
-        res.body.should.be.type('object');
-        done();
-      });
-  });
-
-  it('allows for a user to be retrieved with more detail', function (done) {
-    server
-      .get('/api/v1/users/' + id + '/full')
-      .set('x-access-token', adminToken)
-      .expect(200)
-      .end(function (err, res) {
-        res.status.should.equal(200);
-        res.body.should.be.type('object');
-        done();
-      });
-  });
-
-  it('doesnt allow a non-existent user to be retrieved with detail', function (done) {
-    server
-      .get('/api/v1/users/507f1f77bcf86cd799439011/full')
-      .set('x-access-token', userToken)
-      .expect(400)
-      .end(function (err, res) {
-        res.status.should.equal(400);
-        res.body.message.should.equal('No user exists with that id');
-        done();
-      });
-  });
-
-  it('doesnt allow a non-existent user to be retrieved', function (done) {
-    server
-      .get('/api/v1/users/507f1f77bcf86cd799439011')
-      .set('x-access-token', userToken)
-      .expect(400)
-      .end(function (err, res) {
-        res.status.should.equal(400);
-        res.body.message.should.equal('No user exists with that id');
-        done();
-      });
-  });
-
-  // Update Tests
-  it('allows for users to be updated', function (done) {
-    server
-      .put('/api/v1/users/' + id)
-      .set('x-access-token', userToken)
-      .send({
-        lastName: 'Woke',
-        firstName: 'Wobe',
-        password: 'Youknowwho',
-        username: 'admin',
-        email: 'admin@opinionbucket.io',
-        gender: 'Male',
-        dateOfBirth: '1909-12-12',
-        profilePhoto: 'http://google.com',
-        topics: ['507f1', '507f1f77bcf86cd799439011']
-      })
-      .expect(200)
-      .end(function (err, res) {
-        res.status.should.equal(200);
-        res.body.should.be.type('object');
-        res.body.firstName.should.equal('Wobe');
-        res.body.lastName.should.equal('Woke');
-        done();
-      });
-  });
-
-  it('allows for users to be updated (2)', function (done) {
-    server
-      .put('/api/v1/users/' + id)
-      .set('x-access-token', userToken)
-      .send({
-        lastName: 'Woke',
-        firstName: 'Wobe',
-        password: 'Youknowwho',
-        gender: 'Male',
-        dateOfBirth: '1909-12-12',
-        profilePhoto: 'http://google.com',
-        topics: ['507f1']
-      })
-      .expect(200)
-      .end(function (err, res) {
-        res.status.should.equal(200);
-        res.body.should.be.type('object');
-        res.body.firstName.should.equal('Wobe');
-        res.body.lastName.should.equal('Woke');
-        done();
-      });
-  });
-
-  it('allows for users to be updated (3)', function (done) {
-    server
-      .put('/api/v1/users/' + id)
-      .set('x-access-token', userToken)
-      .send({
-        lastName: 'Woke',
-        firstName: 'Wobe',
-        password: 'Youknowwho',
-        gender: 'Male',
-        dateOfBirth: '1909-12-12',
-        profilePhoto: 'http://google.com',
-        topics: ['507f1f77bcf86cd799439011']
-      })
-      .expect(200)
-      .end(function (err, res) {
-        res.status.should.equal(200);
-        res.body.should.be.type('object');
-        res.body.firstName.should.equal('Wobe');
-        res.body.lastName.should.equal('Woke');
-        done();
-      });
-  });
-
-  it('allows for users to be updated with photos', function (done) {
-    server
-      .put('/api/v1/users/' + id)
-      .set('x-access-token', userToken)
-      .field('lastName', 'Woke')
-      .field('firstName', 'Wobe')
-      .attach('profile', './tests/server/artifacts/sample.png')
-      .expect(200)
-      .end(function (err, res) {
-        res.status.should.equal(200);
-        res.body.should.be.type('object');
-        res.body.firstName.should.equal('Wobe');
-        res.body.lastName.should.equal('Woke');
-        done();
-      });
-  });
-
-  it('does not allow for non-existent users to be updated', function (done) {
-    server
-      .put('/api/v1/users/507f1f77bcf86cd799439011')
-      .set('x-access-token', userToken)
-      .send({
-        lastName: 'Woke'
-      })
-      .expect(404)
-      .end(function (err, res) {
-        res.status.should.equal(404);
-        res.body.should.be.type('object');
-        res.body.message.should.equal('No user with that id');
-        done();
-      });
-  });
-
-  // Delete Tests
-  it('does not allow for the admin to be deleted', function (done) {
-    server
-      .delete('/api/v1/users/' + id)
-      .set('x-access-token', userToken)
-      .expect(403)
-      .end(function (err, res) {
-        res.status.should.equal(403);
-        res.body.message.should.equal('Admin cannot be removed');
-        done();
-      });
-  });
-
-  it('allows for users to be deleted', function (done) {
-    server
-      .get('/api/v1/users')
-      .set('x-access-token', adminToken)
-      .expect(200)
-      .end(function (err, res) {
-        // HACK: get the id of the second user order is different on CI hence the if block
-        if (process.env.NODE_ENV === 'test') {
-          id = res.body[1]._id;
-        } else {
-           id = res.body[0]._id;
-        }
+      it('for  users to be updated', function (done) {
         server
-          .delete('/api/v1/users/' + id)
+          .put('/api/v1/users/' + id)
           .set('x-access-token', userToken)
+          .send({
+            lastName: 'Woke',
+            firstName: 'Wobe',
+            password: 'Youknowwho',
+            username: 'admin',
+            email: 'admin@opinionbucket.io',
+            gender: 'Male',
+            dateOfBirth: '1909-12-12',
+            profilePhoto: 'http://google.com',
+            topics: ['507f1', '507f1f77bcf86cd799439011']
+          })
           .expect(200)
           .end(function (err, res) {
             res.status.should.equal(200);
-            res.body.message.should.equal('User successfully removed');
+            res.body.should.be.type('object');
+            res.body.firstName.should.equal('Wobe');
+            res.body.lastName.should.equal('Woke');
             done();
           });
       });
+
+      it('for  users to be updated (2)', function (done) {
+        server
+          .put('/api/v1/users/' + id)
+          .set('x-access-token', userToken)
+          .send({
+            lastName: 'Woke',
+            firstName: 'Wobe',
+            password: 'Youknowwho',
+            gender: 'Male',
+            dateOfBirth: '1909-12-12',
+            profilePhoto: 'http://google.com',
+            topics: ['507f1']
+          })
+          .expect(200)
+          .end(function (err, res) {
+            res.status.should.equal(200);
+            res.body.should.be.type('object');
+            res.body.firstName.should.equal('Wobe');
+            res.body.lastName.should.equal('Woke');
+            done();
+          });
+      });
+
+      it('for  users to be updated (3)', function (done) {
+        server
+          .put('/api/v1/users/' + id)
+          .set('x-access-token', userToken)
+          .send({
+            lastName: 'Woke',
+            firstName: 'Wobe',
+            password: 'Youknowwho',
+            gender: 'Male',
+            dateOfBirth: '1909-12-12',
+            profilePhoto: 'http://google.com',
+            topics: ['507f1f77bcf86cd799439011']
+          })
+          .expect(200)
+          .end(function (err, res) {
+            res.status.should.equal(200);
+            res.body.should.be.type('object');
+            res.body.firstName.should.equal('Wobe');
+            res.body.lastName.should.equal('Woke');
+            done();
+          });
+      });
+
+      it('for  users to be updated with photos', function (done) {
+        server
+          .put('/api/v1/users/' + id)
+          .set('x-access-token', userToken)
+          .field('lastName', 'Woke')
+          .field('firstName', 'Wobe')
+          .attach('profile', './tests/server/artifacts/sample.png')
+          .expect(200)
+          .end(function (err, res) {
+            res.status.should.equal(200);
+            res.body.should.be.type('object');
+            res.body.firstName.should.equal('Wobe');
+            res.body.lastName.should.equal('Woke');
+            done();
+          });
+      });
+    });
+  });
+
+  describe('deletion', function () {
+    describe('does not allow', function () {
+      it('for  the admin to be deleted', function (done) {
+        server
+          .delete('/api/v1/users/' + id)
+          .set('x-access-token', userToken)
+          .expect(403)
+          .end(function (err, res) {
+            res.status.should.equal(403);
+            res.body.message.should.equal('Admin cannot be removed');
+            done();
+          });
+      });
+    });
+
+    describe('allows', function () {
+      it('for  users to be deleted', function (done) {
+        server
+          .get('/api/v1/users')
+          .set('x-access-token', adminToken)
+          .expect(200)
+          .end(function (err, res) {
+            // HACK: get the id of the second user order is different on CI hence the if block
+            if (process.env.NODE_ENV === 'test') {
+              id = res.body[1]._id;
+            } else {
+              id = res.body[0]._id;
+            }
+            server
+              .delete('/api/v1/users/' + id)
+              .set('x-access-token', userToken)
+              .expect(200)
+              .end(function (err, res) {
+                res.status.should.equal(200);
+                res.body.message.should.equal('User successfully removed');
+                done();
+              });
+          });
+      });
+    });
   });
 });
