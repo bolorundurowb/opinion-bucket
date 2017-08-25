143f94fefe06aab6ce62f3e0a9a95c5208d8c729
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-08-25 12:47:47 +0300

    issue #93 | remove new js file template from code files

--- src/server/routes/Routes.js
+++ src/server/routes/Routes.js
@@ -1,13 +1,12 @@
-/**
- * Created by bolorundurowb on 1/6/17.
- */
-
 import authRoutes from './AuthRoutes';
 import userRoutes from './UserRoutes';
 import categoryRoutes from './CategoryRoutes';
 import topicRoutes from './TopicRoutes';
 import opinionRoutes from './OpinionRoutes';
 
+/**
+ * Holds all routing logic
+ */
 class Routes {
   /**
    * Route handling method
