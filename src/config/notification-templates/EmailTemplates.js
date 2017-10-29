12280398b6ad5a563a4249e4fbbe84bb5d732ac6
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-10-29 21:10:51 +0100

    #118 | add scaffold for generating email object

--- src/config/notification-templates/EmailTemplates.js
+++ src/config/notification-templates/EmailTemplates.js
@@ -1,5 +1,19 @@
-class EmailTemplates {
+import fs from 'fs';
+import path from 'path';
+import ejs from 'ejs';
 
+/**
+ * Handles generating the email objects
+ */
+class EmailTemplates {
+  /**
+   * Returns the email for a new user signup
+   * @param {object}user
+   * @returns {object} - a new user welcome email
+   */
+  static getSignUpMail(user) {
+    return user;
+  }
 }
 
 export default EmailTemplates;
