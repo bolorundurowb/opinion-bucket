648a51f3e9b6fecd30bee218c542063aea139aa8
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-10-29 21:16:08 +0100

    #118 | fix error in config

--- src/config/Config.js
+++ src/config/Config.js
@@ -3,7 +3,7 @@ import cloudinary from 'cloudinary';
 
 const env = process.env.NODE_ENV || 'development';
 if (env !== 'production') {
-  dotenv.Config({ silent: true });
+  dotenv.config({ silent: true });
 }
 
 const Config = {
