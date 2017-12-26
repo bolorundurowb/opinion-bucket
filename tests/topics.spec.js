14ec967b3539ad3fbdfd6111ca509298e5ea2b62
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2017-12-26 01:15:02 +0100

    #131 | initial go at refactoring the tests

--- tests/topics.spec.js
+++ tests/topics.spec.js
@@ -9,6 +9,8 @@ import app from '../src/server';
 
 const server = supertest.agent(app);
 let id = '';
+let topicId = '';
+let opinionId = '';
 let userToken;
 let adminToken;
 
@@ -219,6 +221,253 @@ describe('Topics', () => {
             res.body.message.should.equal('Topic successfully removed');
             done();
           });
+      });
+    });
+  });
+
+  describe('Opinions', () => {
+    before((done) => {
+      server
+        .post('/api/v1/topics')
+        .set('x-access-token', adminToken)
+        .send({ title: 'Sports' })
+        .expect(201)
+        .end((err, res) => {
+          topicId = res.body._id;
+          done();
+        });
+    });
+
+    describe('creation', () => {
+      describe('does not allow', () => {
+        it('for opinions to be without a title', (done) => {
+          server
+            .post(`/api/v1/topics/${topicId}/opinions`)
+            .set('x-access-token', userToken)
+            .send({ topicId })
+            .expect(400)
+            .end((err, res) => {
+              res.status.should.equal(400);
+              res.body.message.should.equal('A title is required.');
+              done();
+            });
+        });
+      });
+
+      describe('allows', () => {
+        it('for opinions to be created', (done) => {
+          server
+            .post(`/api/v1/topics/${topicId}/opinions`)
+            .set('x-access-token', userToken)
+            .send({
+              title: 'Good Stuff',
+              topicId
+            })
+            .expect(201)
+            .end((err, res) => {
+              opinionId = res.body._id;
+              res.status.should.equal(201);
+              res.body.should.be.type('object');
+              res.body.title.should.equal('Good Stuff');
+              done();
+            });
+        });
+      });
+    });
+
+    describe('updating', () => {
+      describe('does not allow', () => {
+        it('for updating non-existent opinions', (done) => {
+          server
+            .put(`/api/v1/topics/${topicId}/opinions/hsiuei`)
+            .set('x-access-token', userToken)
+            .send({})
+            .expect(404)
+            .end((err, res) => {
+              res.status.should.equal(404);
+              res.body.should.be.type('object');
+              res.body.message.should.equal('An opinion with that id doesn\'t exist.');
+              done();
+            });
+        });
+      });
+
+      describe('allows', () => {
+        it('for opinions to be updated', (done) => {
+          server
+            .put(`/api/v1/topics/${topicId}/opinions/${opinionId}`)
+            .set('x-access-token', userToken)
+            .send({
+              title: 'Cool Stuff',
+              showName: true,
+              date: '2015-11-11',
+              content: 'Technology is really good'
+            })
+            .expect(200)
+            .end((err, res) => {
+              res.status.should.equal(200);
+              res.body.should.be.type('object');
+              res.body.title.should.equal('Cool Stuff');
+              res.body.content.should.equal('Technology is really good');
+              done();
+            });
+        });
+      });
+    });
+
+    describe('retrieval', () => {
+      describe('does not allow', () => {
+        it('for a non-existent opinion to be retrieved', (done) => {
+          server
+            .get(`/api/v1/topics/${topicId}/opinions/507f1f77bcf86cd799439011`)
+            .set('x-access-token', userToken)
+            .expect(400)
+            .end((err, res) => {
+              res.status.should.equal(400);
+              res.body.message.should.equal('No opinion exists with that id');
+              done();
+            });
+        });
+      });
+
+      describe('allows', () => {
+        it('for all opinions to be retrieved', (done) => {
+          server
+            .get(`/api/v1/topics/${topicId}/opinions`)
+            .set('x-access-token', userToken)
+            .expect(200)
+            .end((err, res) => {
+              id = res.body[0]._id;
+              res.status.should.equal(200);
+              res.body.should.be.type('object');
+              res.body.length.should.equal(1);
+              done();
+            });
+        });
+
+        it('for all opinions to be retrieved with query options', (done) => {
+          server
+            .get(`/api/v1/topics/${topicId}/opinions?limit=12&offset=0`)
+            .set('x-access-token', userToken)
+            .expect(200)
+            .end((err, res) => {
+              res.status.should.equal(200);
+              res.body.should.be.type('object');
+              res.body.length.should.equal(1);
+              done();
+            });
+        });
+
+        it('for all opinions to be retrieved with other query options', (done) => {
+          server
+            .get(`/api/v1/topics/${topicId}/opinions?order=dislikes`)
+            .set('x-access-token', userToken)
+            .expect(200)
+            .end((err, res) => {
+              res.status.should.equal(200);
+              res.body.should.be.type('object');
+              res.body.length.should.equal(1);
+              done();
+            });
+        });
+
+        it('for all opinions to be retrieved with even more query options', (done) => {
+          server
+            .get(`/api/v1/topics/${topicId}/opinions?author=uyeughst&order=likes`)
+            .set('x-access-token', userToken)
+            .expect(200)
+            .end((err, res) => {
+              res.status.should.equal(200);
+              res.body.should.be.type('object');
+              res.body.length.should.equal(0);
+              done();
+            });
+        });
+
+        it('for an opinion to be retrieved', (done) => {
+          server
+            .get(`/api/v1/topics/${topicId}/opinions/${opinionId}`)
+            .set('x-access-token', userToken)
+            .expect(200)
+            .end((err, res) => {
+              res.status.should.equal(200);
+              res.body.should.be.type('object');
+              done();
+            });
+        });
+      });
+    });
+
+    describe('likes and dislikes', () => {
+      describe('allows', () => {
+        it('for opinions to be liked', (done) => {
+          server
+            .post(`/api/v1/topics/${topicId}/opinions/${opinionId}/like`)
+            .set('x-access-token', userToken)
+            .expect(200)
+            .end((err, res) => {
+              res.status.should.equal(200);
+              res.body.should.be.type('object');
+              res.body.likes.should.equal(1);
+              done();
+            });
+        });
+
+        it('for opinions to be disliked', (done) => {
+          server
+            .post(`/api/v1/topics/${topicId}/opinions/${opinionId}/dislike`)
+            .set('x-access-token', userToken)
+            .expect(200)
+            .end((err, res) => {
+              res.status.should.equal(200);
+              res.body.should.be.type('object');
+              res.body.dislikes.should.equal(1);
+              done();
+            });
+        });
+      });
+
+
+      describe('does not allow', () => {
+        it('for a non-existent opinions to be liked', (done) => {
+          server
+            .post(`/api/v1/topics/${topicId}/opinions/507f1f77bcf86cd799439011/like`)
+            .set('x-access-token', userToken)
+            .expect(404)
+            .end((err, res) => {
+              res.status.should.equal(404);
+              res.body.message.should.equal('An opinion with that id doesn\'t exist.');
+              done();
+            });
+        });
+
+        it('for a non-existent opinions to be disliked', (done) => {
+          server
+            .post(`/api/v1/topics/${topicId}/opinions/507f1f77bcf86cd799439011/dislike`)
+            .set('x-access-token', userToken)
+            .expect(404)
+            .end((err, res) => {
+              res.status.should.equal(404);
+              res.body.message.should.equal('An opinion with that id doesn\'t exist.');
+              done();
+            });
+        });
+      });
+    });
+
+    describe('deletion', () => {
+      describe('allows', () => {
+        it('for opinions to be deleted', (done) => {
+          server
+            .delete(`/api/v1/topics/${topicId}/opinions/${opinionId}`)
+            .set('x-access-token', userToken)
+            .expect(200)
+            .end((err, res) => {
+              res.status.should.equal(200);
+              res.body.message.should.equal('Opinion successfully removed');
+              done();
+            });
+        });
       });
     });
   });
